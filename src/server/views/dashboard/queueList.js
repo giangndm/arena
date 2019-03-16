744e16a5b2eefaac3d72abc894bee5d7313d4a40
Giang Minh <giangndm@ows.vn>
2019-03-16 12:49:00 +0000

    custom manage screen with details of queues

--- src/server/views/dashboard/queueList.js
+++ src/server/views/dashboard/queueList.js
@@ -1,8 +1,17 @@
-function handler(req, res) {
+const QueueHelpers = require('../helpers/queueHelpers');
+
+async function handler(req, res) {
   const {Queues} = req.app.locals;
   const queues = Queues.list();
   const basePath = req.baseUrl;
-
+  
+  let details = await Promise.all(queues.map(async (queue) => {
+      let queue_d = await Queues.get(queue.name, queue.hostId);
+      let counts = await queue_d.getJobCounts();
+      Object.assign(queue, counts);
+      return counts;
+  }));
+  
   return res.render('dashboard/templates/queueList', { basePath, queues });
 }
 
